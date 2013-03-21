3df52703aa33b6f6e155ca66fb886b86d73b27b9
Walker Hale IV <walker.hale.iv@gmail.com>
2013-03-20 22:15:20 -0700

    Update flask_example.py with basic authentication

--- flask_example.py
+++ flask_example.py
@@ -9,15 +9,21 @@ app = Flask(__name__)
 @app.route("/<path:path>")
 def test(path):
     auth = request.authorization
-    username = None
-    password = None
     if auth:
         username = auth.username
         password = auth.password
-    out = {"path": path,
-           "authentication": {"username": username, "password": password}}
-    out['request'] = request.environ
-    return str(('<pre>' + pp.pformat(out) + '</pre>'))
+        out = {"path": path,
+               "authentication": {"username": username, "password": password}}
+        out['request'] = request.environ
+        return str(('<pre>' + pp.pformat(out) + '</pre>'))
+    else:
+        return Response(
+            'TODO: Why access is denied string goes here...\n',
+            401,
+            [('WWW-Authenticate', 'Basic realm="Login Required"'),
+             ('Content-Type', 'text/plain')]
+        )
+
 
 
 if __name__ == "__main__":
