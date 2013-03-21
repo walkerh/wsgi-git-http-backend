80ef7f303f885353376dd4fc3fec84f688382b23
Walker Hale IV <walker.hale.iv@gmail.com>
2013-03-21 13:22:25 -0700

    PEP 8 flask_example.py

--- flask_example.py
+++ flask_example.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, Response
 import pprint
 
-pp = pprint.PrettyPrinter(indent=4)
+from flask import Flask, request, Response
 
+pp = pprint.PrettyPrinter(indent=4)
 app = Flask(__name__)
+
 
 @app.route("/<path:path>")
 def test(path):
@@ -13,13 +14,17 @@ def test(path):
     if auth:
         username = auth.username
         password = auth.password
-    out = {"path":path, "authentication":{"username":username, "password":password}}
+    out = {"path": path,
+           "authentication": {"username": username, "password": password}}
+
     def to_dict(d):
         if isinstance(d, dict):
-            d = {k:to_dict(v) for k,v in d.items()}
+            d = {k: to_dict(v) for k, v in d.items()}
         return d
+
     out['request'] = request.environ
     return str(('<pre>' + pp.pformat(to_dict(out)) + '</pre>'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
