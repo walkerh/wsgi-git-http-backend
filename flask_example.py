75e8a53061bb7257237ce2e38469f7a767bb6c3b
Walker Hale IV <walker.hale.iv@gmail.com>
2013-03-21 13:09:43 -0700

    Add git support to flask_example.py

--- flask_example.py
+++ flask_example.py
@@ -1,12 +1,19 @@
+import os
 import pprint
+import sys
 
 from flask import Flask, request, Response
 
+import gitHttpBackend
+
+
 pp = pprint.PrettyPrinter(indent=4)
 app = Flask(__name__)
+git_project_root = sys.argv[1]
+print git_project_root
 
 
-@app.route("/<path:path>")
+@app.route("/test/<path:path>")
 def test(path):
     auth = request.authorization
     if auth:
@@ -24,6 +31,15 @@ def test(path):
              ('Content-Type', 'text/plain')]
         )
 
+
+@app.route('/git/<path:path>', methods=['GET', 'POST'])
+def git(path):
+    environ = dict(request.environ)
+    environ['PATH_INFO'] = environ['PATH_INFO'][4:]
+    (
+        status_line, headers, response_body_generator
+    ) = gitHttpBackend.wsgi_to_git_http_backend(environ, git_project_root)
+    return Response(response_body_generator, status_line, headers)
 
 
 if __name__ == "__main__":
